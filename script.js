b183f73174d89bb7b60b2cbcf3889d0603fe38d0
daksh <dmathu01@students.cs.ubc.ca>
2025-04-11 02:04:55 -0700

    Shifting to new account

---
+++ script.js
@@ -0,0 +1,34 @@
+function myFunction(){
+    let price = document.querySelector("#price");
+    let planName = document.querySelector("#planName");
+  }
+
+function removeDiv1(btnstate){
+  var eDiv = document.getElementById("a");
+  eDiv.parentNode.removeChild(eDiv);
+}
+
+function removeDiv2(btnstate){
+  var eDiv = document.getElementById("b");
+  eDiv.parentNode.removeChild(eDiv);
+}
+
+function removeDiv3(btnstate){
+  var eDiv = document.getElementById("c");
+  eDiv.parentNode.removeChild(eDiv);
+}
+
+function removeDiv4(btnstate){
+  var eDiv = document.getElementById("d");
+  eDiv.parentNode.removeChild(eDiv);
+}
+
+function removeDiv5(btnstate){
+  var eDiv = document.getElementById("e");
+  eDiv.parentNode.removeChild(eDiv);
+}
+
+function removeDiv6(btnstate){
+  var eDiv = document.getElementById("f");
+  eDiv.parentNode.removeChild(eDiv);
+}
